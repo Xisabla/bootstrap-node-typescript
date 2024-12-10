d4ad34de6671fc08e50fbb9ad23b258d704b5427
Gautier Miquet <xisabla.dev@gmail.com>
2024-12-10 23:45:29 +0100

    first attempt at release script

---
+++ scripts/update-version.js
@@ -0,0 +1,26 @@
+/**
+ * This script is used to update the version of the package.json file.
+ *
+ * It reads the type of bump to perform from the last commit message and updates the version accordingly using `npm version`.
+ *
+ * Commit message format:
+ * - `chore(release): major - ...`
+ *
+ * The allowed types of bumps are:
+ * - major
+ * - minor
+ * - patch
+ * - premajor
+ * - preminor
+ * - prepatch
+ * - prerelease
+ */
+
+const { execSync } = require("child_process");
+
+const commitMessage = execSync("git log -1 --pretty=%B").toString().trim();
+const bumpType = commitMessage.match(
+    /chore\(release\): (major|minor|patch|premajor|preminor|prepatch|prerelease) - /,
+)[1];
+
+execSync(`npm version ${bumpType} --no-git-tag-version`);
